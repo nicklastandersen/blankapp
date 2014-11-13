732759c5332f57554e44ca05d8436231c37e21fd
nicklastandersen <nicklas.t.andersen@gmail.com>
2014-11-13 13:58:06 +0100

    Sandbox
    new

---
+++ Sandbox/InsertCodeHere.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Sandbox
+{
+    class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            int a = 5*7;
+
+            Console.WriteLine("The value of a is : {0}", a);
+
+            int b = 25;
+            b = b * 3;
+            
+            Console.WriteLine("The value of b is : {0}", b);
+
+            int c = 16/5;
+
+            Console.WriteLine("The value of c is : {0}", c);
+
+            double d = 2.5/3.5;
+
+            Console.WriteLine("The value of d is : {0}", d);
+
+            double e = (b - a) + c;
+
+
+            Console.WriteLine("The value of e is : {0}", e);
+
+            double f = 24%6;
+
+
+
+            Console.WriteLine("The value of f is : {0}", f);
+
+            double g = 35;
+            g = 35/f;
+
+            Console.WriteLine("The value of h is : {0}", g);
+
+
+            // The LAST line of code should be Console.WriteLine("The value of h is : {0}", h); this line
+
+        }
+    }
+}
